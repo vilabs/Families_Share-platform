bc00f04597f223689fa1a2acf7c0b4c27061d030
Dimitris Tampakis <dimitamp@auth.gr>
2019-03-29 12:44:22 +0200

    github-route

--- src/routes/github-routes.js
+++ src/routes/github-routes.js
@@ -3,7 +3,8 @@ const router = new express.Router()
 const exec = require('child_process').exec;
 
 router.post('/pushevent', (req, res, next) => {
-	exec('cd ../../' + 'git pull origin master')
+	exec('cd ../../' + 'git pull origin master');
+	console.log('Pulled updated master');
   res.sendStatus(200)
 })
 
