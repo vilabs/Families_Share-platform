37d241303b280655a6b8872ae228ad93888f2095
unknown <dimitamp@auth.gr>
2019-04-01 17:57:04 +0300

    ghr

--- src/routes/github-routes.js
+++ src/routes/github-routes.js
@@ -5,15 +5,8 @@ const exec = require('child_process').exec;
 
 router.post('/pushevent', (req, res, next) => {
 	exec('git pull origin master', (err, stdout, stderr) => {
-		if(err){
-			next(err)
-		}
 		console.log(req.body.payload.commits[0].message)
 		exec('pm2 restart Families_Share', (err2, stdout2, stderr2) => {
-			if (err2) {
-				next
-			}
-			
 			res.sendStatus(200)
 		});
 	})
