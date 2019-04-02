8a67c52665da348ae9aa587b1495fa0f709dc0e9
Dimitris Tampakis <dimitamp@auth.gr>
2019-04-02 14:44:44 +0300

    ghr

--- src/routes/github-routes.js
+++ src/routes/github-routes.js
@@ -3,12 +3,10 @@ const router = new express.Router()
 const exec = require('child_process').exec;
 
 router.post('/pushevent', (req, res, next) => {
-	
 	exec('sh client/sync.sh', (err, stdout, stderr) => {
 		console.log(stdout)
 		console.log(stderr)
 	});
-
 	res.sendStatus(200)
 })
 
