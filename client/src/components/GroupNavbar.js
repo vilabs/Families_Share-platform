307c4fb8abbb1ef16dd3a7ecf58e99a0addd8899
Dimitris Tampakis <dimitamp@auth.gr>
2019-04-03 15:29:20 +0300

    groupnavbar

--- client/src/components/GroupNavbar.js
+++ client/src/components/GroupNavbar.js
@@ -4,77 +4,83 @@ import { withRouter } from 'react-router-dom';
 import Texts from '../Constants/Texts.js';
 import withLanguage from './LanguageContext';
 import Images from '../Constants/Images';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
-class GroupNavbar extends React.Component {
-	handleClick = (event) => {
-		const id = event.currentTarget.id;
-		if (this.props.allowNavigation) {
-			if (event.currentTarget.id === "news") {
-				this.props.history.replace("/groups/" + this.props.match.params.groupId + "/" + id + "/notifications");
+
+const muiTheme = createMuiTheme({
+	typography: {
+		useNextVariants: true
+	},
+	overrides: {
+		MuiBottomNavigation: {
+			root: {
+				position: 'fixed',
+				bottom: 0,
+				height: '5.6rem',
+				backgroundColor: '#00838F',
+				width: '100%',
+			},
+		},
+		MuiBottomNavigationAction: {
+			label: {
+				color: 'white',
+				fontSize: '1.2rem',
+				'&$selected':{
+					fontSize: '1.2rem'
+				}
+			},
+		},
+		MuiButtonBase: {
+			disabled: {
+				opacity: 0.1
 			}
-			else {
-				this.props.history.replace("/groups/" + this.props.match.params.groupId + "/" + id);
-			}
-			this.props.handleActiveTab(id);
-			this.setState({ active: id })
 		}
 	}
+});
 
-	render() {
-		const texts = Texts[this.props.language].groupNavbar;
-		const pathname = this.props.location.pathname;
+
+
+const GroupNavbar = (props) => {
+	const handleChange = (event,value) => {
+		if (props.allowNavigation) {
+			if (value === "news") {
+				props.history.replace("/groups/" + props.match.params.groupId + "/" + value + "/notifications");
+			}
+			else {
+				props.history.replace("/groups/" + props.match.params.groupId + "/" + value);
+			}
+			props.handleActiveTab(value);
+		}
+	}
+		const texts = Texts[props.language].groupNavbar;
+		const pathname = props.location.pathname;
 		let activeTab = pathname.slice(pathname.lastIndexOf("/") + 1, pathname.length);
 		if (activeTab === "notifications" || activeTab === "announcements") {
 			activeTab = "news"
 		}
+		const disabled = !props.allowNavigation;
 		const flags = [
 			activeTab==='info',
 			activeTab==='activities',
 			activeTab==='members',
 			activeTab==='news'
-		]
-		const disabled = !this.props.allowNavigation;
-		return (
-			<nav className="groupNavbarContainer">
-				<ul>
-					<div className="row no-gutters">
-						<div className="col-1-4">
-							<li key="info" id="groupNavbarTab">
-								<button id="info" className="transparentButton" onClick={this.handleClick}>
-									{flags[0]?<i className='fas fa-info-circle'/>:<img src={Images.infoCircleRegular} className="infoCircleRegular" alt="info circle regular" style={disabled? { opacity: 0.1 }:{}} />}
-									<h1>{texts.infoTab}</h1>
-								</button>
-							</li>
-						</div>
-						<div className="col-1-4">
-							<li key="activities" id="groupNavbarTab">
-								<button id="activities" className="transparentButton" onClick={this.handleClick}>
-									<i className={flags[1]?"fas fa-heart":"far fa-heart"} style={disabled? { opacity: 0.1 }:{}} />
-									<h1 style={disabled? { opacity: 0.1 }:{ }}>{texts.activitiesTab}</h1>
-								</button>
-							</li>
-						</div>
-						<div className="col-1-4">
-							<li key="members" id="groupNavbarTab">
-								<button id="members" className="transparentButton" onClick={this.handleClick}>
-								{flags[2]?<i className='fas fa-user-friends'/>:<img src={Images.userFriendsRegular} className="userFriendsRegular" alt="user friends regular" style={disabled? { opacity: 0.1 }:{}} />}
-									<h1 style={disabled? { opacity: 0.1 }:{}}>{texts.membersTab}</h1>
-								</button>
-							</li>
-						</div>
-						<div className="col-1-4">
-							<li key="news" id="groupNavbarTab">
-								<button id="news" className="transparentButton" onClick={this.handleClick}>
-									<i className={flags[3]?"fas fa-envelope":"far fa-envelope"} style={disabled? { opacity: 0.1 }:{}} />
-									<h1 style={disabled? { opacity: 0.1 }:{}}>{texts.newsTab}</h1>
-								</button>
-							</li>
-						</div>
-					</div>
-				</ul>
-			</nav>
-		);
-	}
+	]
+	return (
+		<MuiThemeProvider theme={muiTheme}>
+			<BottomNavigation
+				value={activeTab}
+				onChange={handleChange}
+				showLabels
+			>
+				<BottomNavigationAction value="info" label={texts.infoTab} icon={flags[0] ? <i className="fas fa-info-circle groupNavbarIcon" /> : <img src={Images.infoCircleRegular} className="infoCircleRegular" />} />
+				<BottomNavigationAction value="activities" disabled={disabled} label={texts.activitiesTab} icon={<i className={flags[1] ? "fas fa-heart groupNavbarIcon" : "far fa-heart groupNavbarIcon"} />} />
+				<BottomNavigationAction value="members" disabled={disabled} label={texts.membersTab} icon={flags[2] ? <i className="fas fa-user-friends groupNavbarIcon" /> : <img src={Images.userFriendsRegular} className="userFriendsRegular" />} />
+				<BottomNavigationAction value="news" disabled={disabled} label={texts.newsTab} icon={<i className={flags[3] ? "fas fa-envelope groupNavbarIcon" : "far fa-envelope groupNavbarIcon"} />} />
+			</BottomNavigation>
+		</MuiThemeProvider>
+	);
 }
 
 GroupNavbar.propTypes = {
