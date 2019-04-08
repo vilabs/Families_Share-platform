d21da7cb42771c84762f96607bf2341e690ba946
unknown <dimitamp@auth.gr>
2019-04-08 23:47:09 +0300

    fix

--- src/helper-functions/user-helpers.js
+++ src/helper-functions/user-helpers.js
@@ -1,66 +1,91 @@
-const { google } = require('googleapis')
+const { google } = require("googleapis");
 
-const scopes = 'https://www.googleapis.com/auth/calendar'
-const googleToken = new google.auth.JWT(process.env.GOOGLE_CLIENT_EMAIL, null, process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'), scopes)
+const scopes = "https://www.googleapis.com/auth/calendar";
+const googleToken = new google.auth.JWT(
+  process.env.GOOGLE_CLIENT_EMAIL,
+  null,
+  process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  scopes
+);
 const calendar = google.calendar({
-  version: 'v3',
+  version: "v3",
   auth: googleToken
-})
+});
 
-const getUsersGroupEvents = (calId, userId, usersChildrenIds) => new Promise( async (resolve, reject) => {
-  try {
-    const response = await calendar.events.list({ calendarId: calId });
-    response.data.items.forEach( item => console.log(item.extendedProperties.shared))
-    const usersEvents = response.data.items.filter(event => {
-      const parentIds = JSON.parse(event.extendedProperties.shared.parents);
-      const childrenIds = JSON.parse(
-        event.extendedProperties.shared.children
-      );
-      const fixedFlag =
-        event.extendedProperties.shared.status === "confirmed";
-      const userFlag = parentIds.indexOf(userId) !== -1;
-      const childFlag =
-        usersChildrenIds.filter(
-          childId => childrenIds.indexOf(childId) !== -1
-        ).length > 0;
-      if (fixedFlag && (userFlag || childFlag)) {
-        return true;
-      } else {
-        return false;
+const getUsersGroupEvents = (calId, userId, usersChildrenIds) =>
+  new Promise((resolve, reject) => {
+    calendar.events.list({ calendarId: calId }, function(err, response) {
+      if (err) {
+        reject(err);
       }
-    });
-    console.log(usersEvents)
-    resolve(usersEvents);
-  } catch (error) {
-    resolve([]);
-  }
-})
-
-const unsubcribeChildFromGroupEvents = (calendar_id, child_id) => new Promise(async (resolve, reject) => {
-  try {
-    const resp = await calendar.events.list({ calendarId: calendar_id })
-    const events = resp.data.items.filter(event => event.extendedProperties.shared.status !== 'completed')
-    events.forEach((event) => {
-      const childrenIds = JSON.parse(event.extendedProperties.shared.children)
-      event.extendedProperties.shared.children = JSON.stringify(childrenIds.filter(id => id !== child_id))
-    })
-    await Promise.all(events.map((event) => {
-      const timeslotPatch = {
-        extendedProperties: {
-          shared: {
-            children: event.extendedProperties.shared.children
+      response.data.items.forEach(item =>
+        console.log(item.extendedProperties.shared)
+      );
+      const usersEvents = response.data.items.filter(event => {
+        if (
+          event.extendedProperties.shared.parents !== udnefined &&
+          event.extendedProperties.shared.children !== undefined
+        ) {
+          const parentIds = JSON.parse(event.extendedProperties.shared.parents);
+          const childrenIds = JSON.parse(
+            event.extendedProperties.shared.children
+          );
+          const fixedFlag =
+            event.extendedProperties.shared.status === "confirmed";
+          const userFlag = parentIds.indexOf(userId) !== -1;
+          const childFlag =
+            usersChildrenIds.filter(
+              childId => childrenIds.indexOf(childId) !== -1
+            ).length > 0;
+          if (fixedFlag && (userFlag || childFlag)) {
+            return true;
+          } else {
+            return false;
           }
         }
-      }
-      calendar.events.patch({ calendarId: calendar_id, eventId: event.id, resource: timeslotPatch })
-    }))
-    resolve('done')
-  } catch (error) {
-    reject(error)
-  }
-})
+      });
+      resolve(usersEvents);
+    });
+  });
+
+const unsubcribeChildFromGroupEvents = (calendar_id, child_id) =>
+  new Promise(async (resolve, reject) => {
+    try {
+      const resp = await calendar.events.list({ calendarId: calendar_id });
+      const events = resp.data.items.filter(
+        event => event.extendedProperties.shared.status !== "completed"
+      );
+      events.forEach(event => {
+        const childrenIds = JSON.parse(
+          event.extendedProperties.shared.children
+        );
+        event.extendedProperties.shared.children = JSON.stringify(
+          childrenIds.filter(id => id !== child_id)
+        );
+      });
+      await Promise.all(
+        events.map(event => {
+          const timeslotPatch = {
+            extendedProperties: {
+              shared: {
+                children: event.extendedProperties.shared.children
+              }
+            }
+          };
+          calendar.events.patch({
+            calendarId: calendar_id,
+            eventId: event.id,
+            resource: timeslotPatch
+          });
+        })
+      );
+      resolve("done");
+    } catch (error) {
+      reject(error);
+    }
+  });
 
 module.exports = {
   unsubcribeChildFromGroupEvents,
   getUsersGroupEvents
-}
+};
