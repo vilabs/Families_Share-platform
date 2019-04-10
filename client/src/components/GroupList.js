e70100ae71ac6e8bd5158628a3841acbb61955c7
Dimitris Tampakis <dimitamp@auth.gr>
2019-04-10 14:20:44 +0300

    group search fixed top bar

--- client/src/components/GroupList.js
+++ client/src/components/GroupList.js
@@ -19,7 +19,7 @@ class GroupList extends React.Component {
               indexes = [...Array((block + 1) * 4 <= length ? 4 : length - block * 4).keys()].map(x => block * 4 + x);
             }
             return (
-              <LazyLoad height={350} once offset={100}>
+              <LazyLoad height={350} once offset={150}>
                 {indexes.map(index => (
                   <li key={index} style={{ margin: '1rem 0' }}>
                     <GroupListItem groupId={this.props.groupIds[index]} />
