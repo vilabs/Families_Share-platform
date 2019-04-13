953ce398437de2c6a60592958c144aa4804446e2
unknown <dimitamp@auth.gr>
2019-04-13 11:27:51 +0300

    activitylistitem icon

--- client/src/components/ActivityListItem.js
+++ client/src/components/ActivityListItem.js
@@ -14,7 +14,7 @@ const getUsersChildren = userId => {
 	return axios
 		.get(`/users/${userId}/children`)
 		.then(response => {
-			return response.data;
+			return response.data.map(child => child.child_id);
 		})
 		.catch(error => {
 			console.log(error);
@@ -43,7 +43,7 @@ class ActivityListItem extends React.Component {
 		const userId = JSON.parse(localStorage.getItem('user')).id
 		const groupId = this.props.groupId;
 		const activityId = this.state.activity.activity_id;
-		//const usersChildren = await getUsersChildren(userId);
+		const usersChildren = await getUsersChildren(userId);
 		const timeslots = await getTimeslots(groupId, activityId);
 		const activity = this.state.activity;
 		let dates = timeslots.map(timeslot => timeslot.start.dateTime);
@@ -60,13 +60,22 @@ class ActivityListItem extends React.Component {
 		activity.subscribed = false;
 		for (const timeslot of timeslots){
 			const parents = JSON.parse(timeslot.extendedProperties.shared.parents)
-			//const children = JSON.parse(timeslot.extendedProperties.shared.children)
-			if(parents.includes(userId) ){
+			const children = JSON.parse(timeslot.extendedProperties.shared.children)
+			const userSubscribed = parents.includes(userId)
+			let childrenSubscribed = false;
+			for ( const child of usersChildren ){
+				if(children.includes(child)){
+					childrenSubscribed = true;
+					break;
+				}
+			}
+			if( userSubscribed || childrenSubscribed ){
 				activity.subscribed = true;
 				break;
 			}
 		}
 		activity.dates = uniqueDates;
+		activity.subscribed = true;
 		this.setState({ fetchedTimeslots: true, activity  })
 		}
 		handleActivityClick = (event) => {
@@ -94,48 +103,59 @@ class ActivityListItem extends React.Component {
 
     render() {
 			const activity = this.state.activity
-			return (
-				this.state.fetchedTimeslots?
-				<React.Fragment>
-					<div 
-					  className="row no-gutters" style={{ height: "7rem", cursor: "pointer"}}
-					  id={activity.activity_id} onClick={this.handleActivityClick}
-					>
-					{activity.subscribed &&
-						<i className="fas fa-user-check activityListItemIcon" />
-					}
-						<div className="col-2-10">
-							<i
-				          style={{
-									fontSize: "3rem",
-									color: activity.color
-								}}
-								className="fas fa-certificate center"
-							/>
-						</div>
-						<div className="col-6-10" style={{borderBottom: '1px solid rgba(0,0,0,0.1)'}}>
-							<div className="verticalCenter">
-								<div className="row no-gutters">
-									<h1>{activity.name}</h1>
-								</div>
-								<div className="row no-gutters">
-									<i className="far fa-calendar-alt" style={{marginRight: '1rem'}}/>
-									<h2>
-										{this.getDatesString()}
-									</h2>
-								</div>
-							</div>
-						</div>
-						<div className="col-2-10" style={{borderBottom: '1px solid rgba(0,0,0,0.1)'}}>
-							<i
-								style={{ fontSize: "2rem" }}
-								className="fas fa-chevron-right center"
-							/>
-						</div>
-					</div>
-				</React.Fragment>
-				:<Skeleton avatar active={true} paragraph={{rows: 1}}/>
-			);
+			return this.state.fetchedTimeslots ? (
+        <React.Fragment>
+          <div
+            className="row no-gutters"
+            style={{ height: "7rem", cursor: "pointer" }}
+            id={activity.activity_id}
+            onClick={this.handleActivityClick}
+          >
+            {activity.subscribed && (
+              <div className="activityListItemIcon">
+                <i className="fas fa-user-check" />
+              </div>
+            )}
+            <div className="col-2-10">
+              <i
+                style={{
+                  fontSize: "3rem",
+                  color: activity.color
+                }}
+                className="fas fa-certificate center"
+              />
+            </div>
+            <div
+              className="col-6-10"
+              style={{ borderBottom: "1px solid rgba(0,0,0,0.1)" }}
+            >
+              <div className="verticalCenter">
+                <div className="row no-gutters">
+                  <h1>{activity.name}</h1>
+                </div>
+                <div className="row no-gutters">
+                  <i
+                    className="far fa-calendar-alt"
+                    style={{ marginRight: "1rem" }}
+                  />
+                  <h2>{this.getDatesString()}</h2>
+                </div>
+              </div>
+            </div>
+            <div
+              className="col-2-10"
+              style={{ borderBottom: "1px solid rgba(0,0,0,0.1)" }}
+            >
+              <i
+                style={{ fontSize: "2rem" }}
+                className="fas fa-chevron-right center"
+              />
+            </div>
+          </div>
+        </React.Fragment>
+      ) : (
+        <Skeleton avatar active={true} paragraph={{ rows: 1 }} />
+      );
     }
 }
 
