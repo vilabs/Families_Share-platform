bf3996092cb8f80c3ae5f7ba4af2fb539f191ded
unknown <dimitamp@auth.gr>
2019-04-22 10:45:01 +0300

    Env Samples

--- src/index.js
+++ src/index.js
@@ -1,64 +1,63 @@
-require('dotenv').config();
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-const chalk = require('chalk');
-const jwt = require('jsonwebtoken');
-const compression = require('compression');
+require('dotenv').config()
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const chalk = require('chalk')
+const jwt = require('jsonwebtoken')
+const compression = require('compression')
 
-const port = parseInt(process.env.PORT, 10);
-const config = require('config');
+const port = parseInt(process.env.PORT, 10)
+const config = require('config')
 
-const dbHost = config.get('dbConfig.host');
-mongoose.set('useCreateIndex', true);
-mongoose.set('useNewUrlParser', true);
-mongoose.connect(process.env[dbHost]); // { autoIndex: false } set this to false in production to disable auto creating indexes
-mongoose.Promise = global.Promise;
+const dbHost = config.get('dbConfig.host')
+mongoose.set('useCreateIndex', true)
+mongoose.set('useNewUrlParser', true)
+mongoose.connect(process.env[dbHost]) // { autoIndex: false } set this to false in production to disable auto creating indexes
+mongoose.Promise = global.Promise
 
-const app = express();
+const app = express()
 
 app.use(async (req) => {
   try {
-    const token = req.headers.authorization;
-    const { user_id, email } = await jwt.verify(token, process.env.SERVER_SECRET);
-    req.user_id = user_id;
-    req.email = email;
-    return req.next();
+    const token = req.headers.authorization
+    const { user_id, email } = await jwt.verify(token, process.env.SERVER_SECRET)
+    req.user_id = user_id
+    req.email = email
+    return req.next()
   } catch (e) {
-    return req.next();
+    return req.next()
   }
-});
-app.use(compression());
-if (config.util.getEnv('NODE_ENV') !== 'test') {
-  app.use(morgan('dev'));
+})
+app.use(compression())
+if (config.util.getEnv('NODE_ENV') === 'development') {
+  app.use(morgan('dev'))
 }
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../client/build')));
-app.use('/images', express.static(path.join(__dirname, '../images')));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, '../client/build')))
+app.use('/images', express.static(path.join(__dirname, '../images')))
 
-
-app.use('/groups', require('./routes/group-routes'));
-app.use('/users', require('./routes/user-routes'));
-app.use('/profiles', require('./routes/profile-routes'));
-app.use('/children', require('./routes/child-routes'));
-app.use('/github', require('./routes/github-routes'));
+app.use('/groups', require('./routes/group-routes'))
+app.use('/users', require('./routes/user-routes'))
+app.use('/profiles', require('./routes/profile-routes'))
+app.use('/children', require('./routes/child-routes'))
+app.use('/github', require('./routes/github-routes'))
 
 app.get('/*', (req, res) => {
-  res.sendFile('index.html', { root: path.join(__dirname, '../client/build') });
-});
+  res.sendFile('index.html', { root: path.join(__dirname, '../client/build') })
+})
 
-app.all('*', (req, res) => res.status(404).send('Invalid endpoint'));
+app.all('*', (req, res) => res.status(404).send('Invalid endpoint'))
 
 const server = app.listen(port, () => {
-  console.log(` Server ${chalk.green('started')} at http://localhost:${port}.`);
-});
+  console.log(` Server ${chalk.green('started')} at http://localhost:${port}.`)
+})
 
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something went wrong!');
-});
+  console.error(err.stack)
+  res.status(500).send('Something went wrong!')
+})
 
-module.exports = server;
+module.exports = server
--- test/test.js
+++ test/test.js
@@ -1,31 +1,30 @@
-const common = require('./common');
+const common = require('./common')
 
-const { chai } = common;
-const { server } = common;
+const { chai } = common
+const { server } = common
 
-const Profile = require('../src/models/profile');
-const Address = require('../src/models/address');
-const Image = require('../src/models/image');
-const User = require('../src/models/user');
-const Rating = require('../src/models/rating');
-const Device = require('../src/models/device');
-const Group = require('../src/models/group');
-const Group_Settings = require('../src/models/group-settings');
-const Password_Reset = require('../src/models/password-reset');
-const Member = require('../src/models/member');
-const Notification = require('../src/models/notification');
-const Parent = require('../src/models/parent');
-const Child = require('../src/models/child');
-const Activity = require('../src/models/activity');
-const Day = require('../src/models/day');
-const Announcement = require('../src/models/announcement');
-const Reply = require('../src/models/reply');
+const Profile = require('../src/models/profile')
+const Address = require('../src/models/address')
+const Image = require('../src/models/image')
+const User = require('../src/models/user')
+const Rating = require('../src/models/rating')
+const Device = require('../src/models/device')
+const Group = require('../src/models/group')
+const Group_Settings = require('../src/models/group-settings')
+const Password_Reset = require('../src/models/password-reset')
+const Member = require('../src/models/member')
+const Notification = require('../src/models/notification')
+const Parent = require('../src/models/parent')
+const Child = require('../src/models/child')
+const Activity = require('../src/models/activity')
+const Announcement = require('../src/models/announcement')
+const Reply = require('../src/models/reply')
 
 const importTest = (name, path) => {
   describe(name, () => {
-    require(path);
-  });
-};
+    require(path)
+  })
+}
 
 const initializeDB = async () => {
   const user2 = {
@@ -35,8 +34,8 @@ const initializeDB = async () => {
     email: 'test2@email.com',
     password: 'password',
     visible: true,
-    language: 'en',
-  };
+    language: 'en'
+  }
   const user3 = {
     given_name: 'Test',
     family_name: 'User3',
@@ -44,153 +43,150 @@ const initializeDB = async () => {
     email: 'test3@email.com',
     password: 'password',
     visible: true,
-    language: 'en',
-  };
-  await chai.request(server).post('/users').send(user2);
-  await chai.request(server).post('/users').send(user3);
-  const user = await User.findOne({ email: 'test3@email.com' });
+    language: 'en'
+  }
+  await chai.request(server).post('/users').send(user2)
+  await chai.request(server).post('/users').send(user3)
+  const user = await User.findOne({ email: 'test3@email.com' })
   const group2 = {
     name: 'Test Group 2',
     description: 'Also awesome group',
     visible: true,
     location: 'Kuala lumpur',
     owner_id: user.user_id,
-    invite_ids: [],
-  };
+    invite_ids: []
+  }
   const group3 = {
     name: 'Test Group 3',
     description: 'Again an awesome group',
     visible: true,
     location: 'Kuala lumpur',
     owner_id: user.user_id,
-    invite_ids: [],
-  };
-  await chai.request(server).post('/groups').send(group2).set('Authorization', user.token);
-	await chai.request(server).post('/groups').send(group3).set('Authorization', user.token);
-	const group = await Group.findOne({name: "Test Group 2"});
-	const activity = {
-		information: {
-			name: "Test Activity 2",
-			color: "#00838F",
-			description: "test",
-		},
-		dates: {
-			selectedDays: [
-				'2019-03-06T22:00:00.000Z',
-				'2019-03-13T22:00:00.000Z',
-				'2019-03-20T22:00:00.000Z',
-				'2019-03-27T22:00:00.000Z',
-			],
-			repetition: true,
-			repetitionType: 'weekly'
-		},
-		timeslots: {
-			activityTimeslots: [
-				[
-					{
-						startTime: '20:00',
-						endTime: '22:00',
-						description: 'Test ',
-						name: 'Test Timeslot',
-						requiredParents: 4,
-						requiredChildren: 10,
-						cost: 10,
-						location: 'Kuala lumpur'
-					}
-				],
-				[
-					{
-						startTime: '20:00',
-						endTime: '22:00',
-						description: 'Test ',
-						name: 'Test Timeslot',
-						requiredParents: 4,
-						requiredChildren: 10,
-						cost: 10,
-						location: 'Kuala lumpur'
-					}
-				],
-				[
-					{
-						startTime: '20:00',
-						endTime: '22:00',
-						description: 'Test ',
-						name: 'Test Timeslot',
-						requiredParents: 4,
-						requiredChildren: 10,
-						cost: 10,
-						location: 'Kuala lumpur'
-					}
-				],
-				[
-					{
-						startTime: '20:00',
-						endTime: '22:00',
-						description: 'Test ',
-						name: 'Test Timeslot',
-						requiredParents: 4,
-						requiredChildren: 10,
-						cost: 10,
-						location: 'Kuala lumpur'
-					}
-				]
-			],
-			differentTimeslots: false,
-		}
-	};
-	await chai.request(server).post(`/groups/${group.group_id}/activities`).send(activity).set('Authorization', user.token);
-	const announcement = {
-		message: "Test Announcement 2"
-	};
-	await chai.request(server).post(`/groups/${group.group_id}/announcements`).send(announcement).set('Authorization',user.token);
-	const child = {
+    invite_ids: []
+  }
+  await chai.request(server).post('/groups').send(group2).set('Authorization', user.token)
+  await chai.request(server).post('/groups').send(group3).set('Authorization', user.token)
+  const group = await Group.findOne({ name: 'Test Group 2' })
+  const activity = {
+    information: {
+      name: 'Test Activity 2',
+      color: '#00838F',
+      description: 'test'
+    },
+    dates: {
+      selectedDays: [
+        '2019-03-06T22:00:00.000Z',
+        '2019-03-13T22:00:00.000Z',
+        '2019-03-20T22:00:00.000Z',
+        '2019-03-27T22:00:00.000Z'
+      ],
+      repetition: true,
+      repetitionType: 'weekly'
+    },
+    timeslots: {
+      activityTimeslots: [
+        [
+          {
+            startTime: '20:00',
+            endTime: '22:00',
+            description: 'Test ',
+            name: 'Test Timeslot',
+            requiredParents: 4,
+            requiredChildren: 10,
+            cost: 10,
+            location: 'Kuala lumpur'
+          }
+        ],
+        [
+          {
+            startTime: '20:00',
+            endTime: '22:00',
+            description: 'Test ',
+            name: 'Test Timeslot',
+            requiredParents: 4,
+            requiredChildren: 10,
+            cost: 10,
+            location: 'Kuala lumpur'
+          }
+        ],
+        [
+          {
+            startTime: '20:00',
+            endTime: '22:00',
+            description: 'Test ',
+            name: 'Test Timeslot',
+            requiredParents: 4,
+            requiredChildren: 10,
+            cost: 10,
+            location: 'Kuala lumpur'
+          }
+        ],
+        [
+          {
+            startTime: '20:00',
+            endTime: '22:00',
+            description: 'Test ',
+            name: 'Test Timeslot',
+            requiredParents: 4,
+            requiredChildren: 10,
+            cost: 10,
+            location: 'Kuala lumpur'
+          }
+        ]
+      ],
+      differentTimeslots: false
+    }
+  }
+  await chai.request(server).post(`/groups/${group.group_id}/activities`).send(activity).set('Authorization', user.token)
+  const announcement = {
+    message: 'Test Announcement 2'
+  }
+  await chai.request(server).post(`/groups/${group.group_id}/announcements`).send(announcement).set('Authorization', user.token)
+  const child = {
     given_name: 'Test',
     family_name: 'Child',
     gender: 'girl',
     birthdate: new Date(),
     allergies: 'allergic to peanuts',
     special_needs: 'no',
-    other_info: 'no',
-  };
-  await chai.request(server).post(`/users/${user.user_id}/children`).send(child).set('Authorization', user.token);
-	
-};
+    other_info: 'no'
+  }
+  await chai.request(server).post(`/users/${user.user_id}/children`).send(child).set('Authorization', user.token)
+}
 describe('Test', () => {
   before('Initializing DB', async () => {
-    await initializeDB();
-	});
-	
-  importTest('User Endpoints Test', './Users/userEndpoints');
-  importTest('Group Endpoints Test', './Groups/groupEndpoints');
-  importTest('Users Groups Endpoints Test', './Users/groupEndpoints');
-  importTest('Users Profile Endpoints Test', './Users/profileEndpoints');
-  importTest('Users Children Endpoints Test', './Users/childrenEndpoints');
-  importTest('Group Members Endpoints Test', './Groups/memberEndpoints');
-	importTest('Group Various Endpoints Test', './Groups/variousEndpoints');
-	importTest('Group Various Endpoints Test', './Groups/activityEndpoints');
-	importTest('Group Announcement Endpoints Test', './Groups/announcementEndpoints');
-	importTest('User Various Endpoints Test', './Users/variousEndpoints');
-	importTest('Child Endpoints Test', './Children/childEndpoints');
-	importTest('Profile Endpoints Test', './Profiles/profileEndpoints');
+    await initializeDB()
+  })
 
+  importTest('User Endpoints Test', './Users/userEndpoints')
+  importTest('Group Endpoints Test', './Groups/groupEndpoints')
+  importTest('Users Groups Endpoints Test', './Users/groupEndpoints')
+  importTest('Users Profile Endpoints Test', './Users/profileEndpoints')
+  importTest('Users Children Endpoints Test', './Users/childrenEndpoints')
+  importTest('Group Members Endpoints Test', './Groups/memberEndpoints')
+  importTest('Group Various Endpoints Test', './Groups/variousEndpoints')
+  importTest('Group Various Endpoints Test', './Groups/activityEndpoints')
+  importTest('Group Announcement Endpoints Test', './Groups/announcementEndpoints')
+  importTest('User Various Endpoints Test', './Users/variousEndpoints')
+  importTest('Child Endpoints Test', './Children/childEndpoints')
+  importTest('Profile Endpoints Test', './Profiles/profileEndpoints')
 
   after('Cleaning up', async () => {
-    await User.deleteMany({});
-    await Profile.deleteMany({});
-    await Image.deleteMany({});
-    await Rating.deleteMany({});
-    await Address.deleteMany({});
-    await Device.deleteMany({});
-    await Password_Reset.deleteMany({});
-    await Group.deleteMany({});
-    await Group_Settings.deleteMany({});
-    await Member.deleteMany({});
-    await Notification.deleteMany({});
-    await Parent.deleteMany({});
-		await Child.deleteMany({});
-		await Activity.deleteMany({});
-		await Day.deleteMany({});
-		await Reply.deleteMany({});
-		await Announcement.deleteMany({});
-  });
-});
+    await User.deleteMany({})
+    await Profile.deleteMany({})
+    await Image.deleteMany({})
+    await Rating.deleteMany({})
+    await Address.deleteMany({})
+    await Device.deleteMany({})
+    await Password_Reset.deleteMany({})
+    await Group.deleteMany({})
+    await Group_Settings.deleteMany({})
+    await Member.deleteMany({})
+    await Notification.deleteMany({})
+    await Parent.deleteMany({})
+    await Child.deleteMany({})
+    await Activity.deleteMany({})
+    await Reply.deleteMany({})
+    await Announcement.deleteMany({})
+  })
+})
