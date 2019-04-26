b6c2621425a48501062a5d59ee82dc8faa0ef4f9
unknown <dimitamp@auth.gr>
2019-04-26 13:37:29 +0300

    forgot password issue

--- src/helper-functions/forgot-password-email.js
+++ src/helper-functions/forgot-password-email.js
@@ -12,7 +12,7 @@ function newForgotPasswordEmail (token) {
         We got a request to change your password.
       </p>
       <a
-        href="http://83.212.107.194:4000/users/api/changepasswordredirect/${token}"
+        href="http://83.212.107.194:4000/api/users/changepasswordredirect/${token}"
         style="text-decoration:none"
       >
         <button
