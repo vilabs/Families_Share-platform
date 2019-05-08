6f53768964499bb5c71a254816e2b2adad77c021
Dimitris Tampakis <dimitamp@auth.gr>
2019-05-08 11:25:38 +0300

    force https

--- src/index.js
+++ src/index.js
@@ -26,7 +26,9 @@ app.enable('trust proxy');
 
 if (process.env.CITYLAB !== 'ALL') {
 	app.use(function (req, res, next) {
-		if (!req.secure) {
+		if (req.secure) {
+			next()
+		} else {
 			res.redirect('https://' + req.headers.host + req.url);
 		}
 	});
