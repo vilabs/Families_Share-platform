21b7260c298251f4f15e88efe939343293ee3f6b
Dimitris Tampakis <dimitamp@auth.gr>
2019-05-15 15:01:22 +0300

    Language Issue

--- client/src/components/SelectLanguage.js
+++ client/src/components/SelectLanguage.js
@@ -1,65 +1,67 @@
 import React from "react";
 import { Menu, Dropdown } from "antd";
-import { languages } from "../Constants/GlobalVars.js";
+import PropTypes from "prop-types";
+import { languages } from "../Constants/GlobalVars";
 import LanguageIcon from "./LanguageIcon";
 import withLanguage from "./LanguageContext";
 
-class SelectLanguage extends React.Component {
-  state = { language: this.props.language };
-
-  handleClick = e => {
-    this.props.updateLanguage(e.key);
+const SelectLanguage = ({ language, updateLanguage }) => {
+  const handleClick = e => {
+    updateLanguage(e.key);
   };
 
-  render() {
-    const menuStyle = {
-      width: "4rem",
-      borderRadius: "20px",
-      backgroundColor: "transparent",
-      boxShadow: "none",
-      WebkitBoxShadow: "none",
-      MozBoxShadow: "none"
-    };
-    const menuItemStyle = {
-      padding: "0.3rem 0"
-    };
-    const languageIconStyle = {
-      position: "relative",
-      left: "0"
-		};
-		let appLanguages;
-		if(process.env.REACT_APP_CITYLAB === "all"){
-			appLanguages =languages;
-		} else {
-			const defaultLang = process.env.REACT_APP_CITYLAB_DEFAULT_LANG;
-			const alternativeLang = process.env.REACT_APP_CITYLAB_ALTERNATIVE_LANG;
-			if (defaultLang && alternativeLang) {
-				appLanguages = [defaultLang, alternativeLang];
-			} else {
-				appLanguages = ["defaultLang"]
-			}
-		}
-    const menu = (
-      <Menu style={menuStyle} onClick={this.handleClick}>
-        {appLanguages.map(language =>
-          language !== this.props.language ? (
-            <Menu.Item key={language} style={menuItemStyle}>
-              <LanguageIcon style={languageIconStyle} language={language} />
-            </Menu.Item>
-          ) : (
-            ""
-          )
-        )}
-      </Menu>
-    );
-    return (
-      <Dropdown trigger={["hover", "click"]} overlay={menu}>
-        <div className="ant-dropdown-link">
-          <LanguageIcon style={menuItemStyle} language={this.props.language} />
-        </div>
-      </Dropdown>
-    );
+  const menuStyle = {
+    width: "4rem",
+    borderRadius: "20px",
+    backgroundColor: "transparent",
+    boxShadow: "none",
+    WebkitBoxShadow: "none",
+    MozBoxShadow: "none"
+  };
+  const menuItemStyle = {
+    padding: "0.3rem 0"
+  };
+  const languageIconStyle = {
+    position: "relative",
+    left: "0"
+  };
+  let appLanguages;
+  if (process.env.REACT_APP_CITYLAB === "all") {
+    appLanguages = languages;
+  } else {
+    const defaultLang = process.env.REACT_APP_CITYLAB_DEFAULT_LANG;
+    const alternativeLang = process.env.REACT_APP_CITYLAB_ALTERNATIVE_LANG;
+    if (defaultLang && alternativeLang) {
+      appLanguages = [defaultLang, alternativeLang];
+    } else {
+      appLanguages = [defaultLang];
+    }
   }
-}
+  const menu = (
+    <Menu style={menuStyle} onClick={handleClick}>
+      {appLanguages.map(lang =>
+        lang !== language ? (
+          <Menu.Item key={lang} style={menuItemStyle}>
+            <LanguageIcon style={languageIconStyle} language={lang} />
+          </Menu.Item>
+        ) : (
+          ""
+        )
+      )}
+    </Menu>
+  );
+  return (
+    <Dropdown trigger={["hover", "click"]} overlay={menu}>
+      <div className="ant-dropdown-link">
+        <LanguageIcon style={menuItemStyle} language={language} />
+      </div>
+    </Dropdown>
+  );
+};
+
+SelectLanguage.propTypes = {
+  language: PropTypes.string,
+  updateLanguage: PropTypes.func
+};
 
 export default withLanguage(SelectLanguage);
