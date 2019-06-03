f2b7005c54170062d3fb3e1f889b6b8594d565ab
unknown <dimitamp@auth.gr>
2019-06-03 17:58:01 +0300

    intro slider

--- client/src/components/MyFamiliesShareScreen.js
+++ client/src/components/MyFamiliesShareScreen.js
@@ -2,6 +2,7 @@ import React from "react";
 import axios from "axios";
 import moment from "moment";
 import PropTypes from "prop-types";
+import IntroSlider from "react-intro-slider";
 import MyFamiliesShareHeader from "./MyFamiliesShareHeader";
 import withLanguage from "./LanguageContext";
 import GroupList from "./GroupList";
@@ -51,7 +52,8 @@ class MyFamiliesShareScreen extends React.Component {
       fetchedUserInfo: false,
       myTimeslots: [],
       myGroups: [],
-      pendingInvites: 0
+      pendingInvites: 0,
+      tutorialIsOpen: true
     };
   }
 
@@ -88,6 +90,10 @@ class MyFamiliesShareScreen extends React.Component {
       pendingInvites
     });
   }
+
+  handleTutorialClose = () => {
+    this.setState({ tutorialIsOpen: false });
+  };
 
   renderGroupSection = () => {
     const { language } = this.props;
@@ -143,21 +149,75 @@ class MyFamiliesShareScreen extends React.Component {
   };
 
   render() {
-    const { pendingInvites, unreadNotifications, fetchedUserInfo } = this.state;
+    const slides = [
+      {
+        title: "Welcome to Families Share",
+        image:
+          "https://lh3.googleusercontent.com/fZPCbd5yGLTleONbeHZaEtZueFrnLmTzA8fq3PqP5M4cgAssOAdd0Y4zkctDBOJAI-UA",
+        description:
+          "A social networking and awareness-raising platform dedicated to encouraging childcare and work/life balance.",
+        background: "#ADD8E6"
+      },
+      {
+        title: "Profile Customizing",
+        image: "https://sitejerk.com/images/profile-image-png-17.png",
+        description:
+          "Edit your personal information and upload new profile photos"
+      },
+      {
+        title: "Create and Join Groups",
+        image: "http://www.pngmart.com/files/7/Community-PNG-Picture.png",
+        background: "#ADD8E6",
+        description: "Create/Join groups and connect with people"
+      },
+      {
+        title: "Create and Join Activities",
+        image:
+          "https://media.time4learning.com/uploads/special-needs-activities-header.png",
+        description: "Organize interesting and fun activities for your children"
+      }
+    ];
+    const {
+      pendingInvites,
+      unreadNotifications,
+      fetchedUserInfo,
+      tutorialIsOpen
+    } = this.state;
     return (
-      <div id="drawerContainer">
-        <MyFamiliesShareHeader
-          pendingInvites={pendingInvites}
-          pendingNotifications={unreadNotifications}
-        />
-        {fetchedUserInfo && (
-          <div id="myFamiliesShareMainContainer">
-            {this.renderGroupSection()}
-            {this.renderTimeslotsSection()}
-            {this.renderPromptAction()}
-          </div>
-        )}
-      </div>
+      <React.Fragment>
+        <div style={{ zIndex: 101, position: "absolute" }}>
+          <IntroSlider
+            slides={slides}
+            skipButton
+            handleClose={this.handleTutorialClose}
+            sliderIsOpen={tutorialIsOpen}
+            size="large"
+            titleStyle={{
+              fontSize: "2rem",
+              color: "rgba(0,0,0,0.7)",
+              fontFamily: "'Roboto', sans-serif"
+            }}
+            descriptionStyle={{
+              fontSize: "1.6rem",
+              color: "rgba(0,0,0,0.7)",
+              fontFamily: "'Roboto', sans-serif"
+            }}
+          />
+        </div>
+        <div id="drawerContainer">
+          <MyFamiliesShareHeader
+            pendingInvites={pendingInvites}
+            pendingNotifications={unreadNotifications}
+          />
+          {fetchedUserInfo && (
+            <div id="myFamiliesShareMainContainer">
+              {this.renderGroupSection()}
+              {this.renderTimeslotsSection()}
+              {this.renderPromptAction()}
+            </div>
+          )}
+        </div>
+      </React.Fragment>
     );
   }
 }
