e604fbffbfdac8c363e040d934350032bcd26280
unknown <dimitamp@auth.gr>
2019-06-10 20:08:37 +0300

    push notifications

--- src/routes/user-routes.js
+++ src/routes/user-routes.js
@@ -997,18 +997,18 @@ router.post('/:userId/sendmenotification', async (req, res, next) => {
       return res.status(404).send('User has no registered devices')
     }
     const messages = []
-    const invalidTokens = []
+    // const invalidTokens = []
     devices.forEach((device) => {
-      if (Expo.isExpoPushToken(device.device_id)) {
-        messages.push({
-          to: device.device_id,
-          sound: 'default',
-          title: 'Welcome',
-          body: 'Families Share welcomes you to our community'
-        })
-      } else {
-        invalidTokens.push(device.device_id)
-      }
+      // if (Expo.isExpoPushToken(device.device_id)) {
+      messages.push({
+        to: device.device_id,
+        sound: 'default',
+        title: 'Welcome',
+        body: 'Families Share welcomes you to our community'
+      })
+      // } else {
+      // invalidTokens.push(device.device_id)
+      // }
     })
     let chunks = expo.chunkPushNotifications(messages)
     let tickets = []
@@ -1016,7 +1016,7 @@ router.post('/:userId/sendmenotification', async (req, res, next) => {
       let ticketChunk = await expo.sendPushNotificationsAsync(chunk)
       tickets.push(...ticketChunk)
     }
-    await Device.deleteMany({ device_id: { $in: invalidTokens } })
+    // await Device.deleteMany({ device_id: { $in: invalidTokens } })
     res.status(200).send('Push notification sent')
   } catch (err) {
     next(err)
