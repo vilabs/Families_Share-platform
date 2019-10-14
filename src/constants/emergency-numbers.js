f6332b76515c94dcf26ef6af60f6fe6e3909ef5d
Dimitris Tampakis <dimitamp@auth.gr>
2019-10-14 11:16:51 +0300

    Added emergency numbers

---
+++ src/constants/emergency-numbers.js
@@ -0,0 +1,82 @@
+const Italy = [
+  {
+    service: 'Ambulance/first aid',
+    number: '118'
+  },
+  {
+    service: 'Police',
+    number: '113'
+  },
+  {
+    service: 'Fire department',
+    number: '115'
+  },
+  {
+    service: 'Unique number of emergency',
+    number: '112'
+  }
+]
+
+const Belgium = [
+  {
+    service: 'Red cross',
+    number: '105'
+  },
+  {
+    service: 'Police',
+    number: '101'
+  },
+  {
+    service: 'Fire department',
+    number: '100'
+  },
+  {
+    service: 'Unique number of emergency',
+    number: '112'
+  }
+]
+
+const Greece = [
+  {
+    service: 'Ambulance/first aid',
+    number: '166'
+  },
+  {
+    service: 'Police',
+    number: '100'
+  },
+  {
+    service: 'Fire department',
+    number: '199'
+  },
+  {
+    service: 'Unique number of emergency',
+    number: '112'
+  }
+]
+
+const Hungary = [
+  {
+    service: 'Ambulance/first aid',
+    number: '104'
+  },
+  {
+    service: 'Police',
+    number: '107'
+  },
+  {
+    service: 'Fire department',
+    number: '105'
+  },
+  {
+    service: 'Unique number of emergency',
+    number: '112'
+  }
+]
+
+module.exports = {
+  Italy,
+  Belgium,
+  Greece,
+  Hungary
+}
