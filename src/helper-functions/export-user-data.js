0fd1c46702196bddbd4b597f693a24bf9d3318e5
Dimitris Tampakis <dimitamp@auth.gr>
2019-10-22 11:15:26 +0300

    Updated User Data Pdf Export

--- src/helper-functions/export-user-data.js
+++ src/helper-functions/export-user-data.js
@@ -1,7 +1,7 @@
-const PDFDocument = require('pdfkit')
 const fs = require('fs')
 const moment = require('moment')
 const path = require('path')
+var PdfMake = require('pdfmake')
 
 function newExportEmail (given_name) {
   return (`<div
@@ -23,71 +23,125 @@ function newExportEmail (given_name) {
 }
 
 function createPdf (profile, groups, children, events, cb) {
-  const doc = new PDFDocument({ autoFirstPage: false })
-  doc.pipe(fs.createWriteStream(`${profile.given_name.toUpperCase()}_${profile.family_name.toUpperCase()}.pdf`))
-  doc.addPage({ margin: 50 })
+  const fonts = {
+    Roboto: {
+      normal: path.join(__dirname, '../fonts', 'Roboto-Regular.ttf'),
+      bold: path.join(__dirname, '../fonts', 'Roboto-Bold.ttf'),
+      italics: path.join(__dirname, '../fonts', 'Roboto-Italic.ttf'),
+      bolditalics: path.join(__dirname, '../fonts', 'Roboto-BoldItalic.ttf')
+    }
+  }
+  const printer = new PdfMake(fonts)
 
-  doc.fontSize(18).text(`User's Profile`, {
-    align: 'center'
-  })
-  doc.image(path.join(__dirname, `../..${profile.image.path}`), { width: 150 })
-  doc.moveDown()
-  doc.fontSize(14)
-  doc.text(`Given name: ${profile.given_name}`, {
-    align: 'left' })
-  doc.text(`Family name: ${profile.family_name}`, {
-    align: 'left' })
-  doc.text(`Email: ${profile.email}`, {
-    align: 'left' })
-  doc.text(`Number: ${profile.phone}`, {
-    align: 'left' })
-  doc.text(`Address: ${profile.address.city}, ${profile.address.street} ${profile.address.number}`, {
-    align: 'left' })
-  doc.moveDown()
-  doc.fontSize(18)
-  doc.text(`User's Groups`, {
-    align: 'center'
-  })
-  doc.fontSize(14)
-  groups.forEach((group, index) => {
-    doc.text(`${index + 1}. ${group.name}`, {
-      align: 'left' })
-  })
-  doc.moveDown()
-  doc.fontSize(18)
-  doc.text(`User's Children`, {
-    align: 'center'
-  })
-  doc.fontSize(14)
-  children.forEach(child => {
-    doc.image(path.join(__dirname, `../..${child.image.path}`), { width: 150, heigh: 150 })
-    doc.moveDown()
-    doc.text(`Given name: ${child.given_name}`, {
-      align: 'left' })
-    doc.text(`Family name: ${child.family_name}`, {
-      align: 'left' })
-    doc.text(`Gender: ${child.gender}`, {
-      align: 'left' })
-    doc.text(`Birthday: ${moment(child.birthdate).format('DD MMMM YYYY')}`, {
-      align: 'left' })
-    doc.text(`Allergies: ${child.allergies}`, {
-      align: 'left' })
-    doc.text(`Special needs: ${child.special_needs}`, {
-      align: 'left' })
-    doc.text(`Other info: ${child.other_info}`, {
-      align: 'left' })
-    doc.moveDown()
-  })
-  doc.fontSize(18)
-  doc.text(`User's Events`, {
-    align: 'center'
-  })
-  doc.fontSize(14)
-  events.forEach((event, index) => {
-    doc.text(`${index + 1}. ${event.summary}`, {
-      align: 'left' })
-  })
-  doc.end()
+  const docDefinition = {
+    pageMargins: 50,
+    header: {
+      columns: [
+        {
+          text: 'Families Share Platform',
+          alignment: 'right',
+          opacity: 0.5
+        }
+      ]
+    },
+    content: [
+      {
+        text: 'User Data Export',
+        alignment: 'center',
+        fontSize: 16,
+        margin: [0, 0, 0, 20],
+        bold: true
+      },
+      {
+        text: 'Profile',
+        fontSize: 14,
+        bold: true,
+        margin: [0, 0, 0, 20]
+      },
+      {
+        margin: [0, 0, 0, 20],
+        columns: [
+          {
+            width: '40%',
+            image: path.join(__dirname, `../..${profile.image.path}`),
+            fit: [170, 170]
+          },
+          {
+            width: '60%',
+            stack: [
+              `Given name: ${profile.given_name}`,
+              `Family name: ${profile.family_name}`,
+              `Description: ${profile.description ? profile.description : '-'}`,
+              `Email: ${profile.email}`,
+              `Phone: ${profile.phone ? profile.phone : '-'}`,
+              `Address: ${profile.address.city}, ${profile.address.street} ${profile.address.number}`
+            ],
+            fontSize: 14
+          }
+        ]
+      },
+      {
+        text: 'Groups',
+        fontSize: 14,
+        bold: true,
+        margin: [0, 0, 0, 20]
+      },
+      {
+        margin: [0, 0, 0, 20],
+        ol: groups.map(group => ({
+          text: `${group.name}: ${group.description}`,
+          fontSize: 14
+        }))
+      },
+      {
+        text: 'Children',
+        fontSize: 14,
+        bold: true,
+        margin: [0, 0, 0, 20]
+      },
+      ...children.map(child => ({
+        margin: [0, 0, 0, 20],
+        columns: [
+          {
+            width: '40%',
+            image: path.join(__dirname, `../..${child.image.path}`),
+            fit: [170, 170]
+          },
+          {
+            width: '60%',
+            stack: [
+              `Given name: ${child.given_name}`,
+              `Family name: ${child.family_name}`,
+              `Gender: ${child.gender}`,
+              `Email: ${profile.email}`,
+              `Birthday: ${moment(child.birthdate).format('DD MMMM YYYY')}`,
+              `Allergies: ${child.allergies ? child.allergies : '-'}`,
+              `Special Needs: ${child.special_needs ? child.special_needs : '-'}`,
+              `Other Info: ${child.other_info ? child.other_info : '-'}`
+            ],
+            fontSize: 14
+          }
+        ]
+      })),
+      {
+        text: 'Activities',
+        fontSize: 14,
+        bold: true,
+        margin: [0, 0, 0, 20]
+      },
+      {
+        margin: [0, 0, 0, 20],
+        ol: events.map(event => ({
+          text: `${event.summary}: ${event.description}`,
+          fontSize: 14
+        }))
+      }
+
+    ]
+  }
+  const pdfDoc = printer.createPdfKitDocument(docDefinition)
+  pdfDoc.pipe(fs.createWriteStream(`${profile.given_name.toUpperCase()}_${profile.family_name.toUpperCase()}.pdf`))
+  pdfDoc.end()
   cb()
 }
 
